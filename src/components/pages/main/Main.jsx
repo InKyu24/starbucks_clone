d3ed50cfd8386bfbeb62734d2b69ebbd43c82ff7
InKyu <484342@gmail.com>
2023-01-28 21:16:35 +0900

    add moving image in the  main page(using lottie)

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Lottie from 'lottie-react';
+import mainAnimation from '../../../images/mainImg.json';
 
 function Main() {
     return ( 
         <div className="container">
             <h1>Main Page</h1>
+            <Lottie animationData={mainAnimation} style={{height:"200px"}}/>
         </div>
      );
 }
